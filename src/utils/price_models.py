92793410b6b26a2c0252415cc137ee2f6d53777d
ge__ <g.ecrepont@gmail.com>
2024-04-01 22:41:32 +0200

    ça avance les estimateurs!

---
+++ src/utils/price_models.py
@@ -0,0 +1,94 @@
+# Implements various price models
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class GBM:
+    """Model price using geometric brownian motion."""
+    def __init__(self, mu: float, sigma: float):
+        self.mu = mu
+        self.sigma = sigma
+
+    def simulate(self, T: float, dt: float, S0: float=100) -> np.ndarray:
+        """
+        Simulate a price path using brownian motion.
+        T and dt are expressed in years. (because mu and sigma are annualized)
+        """
+        N = int(T/dt)
+        Z = np.random.normal(size=N)
+        delta_ln_S = (self.mu - 0.5*self.sigma**2)*dt + self.sigma*np.sqrt(dt)*Z
+        ln_S = np.cumsum(delta_ln_S)
+        ln_S = np.insert(ln_S, 0, 0)
+        S = S0*np.exp(ln_S)
+        # turn S into time series
+        timestep = pd.Timedelta(dt*365, 'D')
+        index = pd.date_range(start=pd.Timestamp('2024-01-01'), periods=N+1, freq=timestep)
+        S = pd.Series(S, index=index)
+        return S
+    
+    def plot(self, S: np.ndarray) -> None:
+        plt.figure()
+        plt.plot(S)
+        plt.xlabel('Time')
+        plt.ylabel('Price')
+        # rotate xticks
+        plt.xticks(rotation=45)
+        plt.title(f'GBM Price Path ($\\mu={self.mu}, \\sigma={self.sigma}$)')
+        plt.show()
+    
+
+class Heston:
+    """
+    Model price using Heston stochastic volatility model.
+    
+    dS_t/S_t = mu*dt + sqrt(nu_t)*dW_1
+    dnu_t = kappa*(theta - nu_t)*dt + sigma*sqrt(nu_t)*dW_2
+
+    T and dt are expressed in years. (because mu and sigma are annualized)
+    """
+    def __init__(self, mu: float, sigma: float, theta: float, rho: float, kappa: float, nu0: float):
+        assert 2*kappa*theta > sigma**2, 'Feller condition not satisfied.'
+        self.mu = mu
+        self.kappa = kappa
+        self.theta = theta
+        self.sigma = sigma
+        self.rho = rho
+        self.nu0 = nu0
+
+    def simulate(self, T: float, dt: float, S0: float=100) -> tuple[np.ndarray, np.ndarray]:
+        """Simulate a price path using Heston model."""
+        N = int(T/dt)
+        Z = np.random.normal(size=N)
+        W1 = np.random.normal(size=N)
+        W2 = np.sqrt(1-self.rho**2)*Z + self.rho*W1
+        nu = np.zeros(N)
+        nu[0] = self.nu0
+        S = np.zeros(N)
+        S[0] = S0
+        for i in range(1, N):
+            nu[i] = nu[i-1] + self.kappa*(self.theta - nu[i-1])*dt + self.sigma*np.sqrt(nu[i-1]*dt)*W1[i]
+            S[i] = S[i-1]*np.exp((self.mu - 0.5*nu[i])*dt + np.sqrt(nu[i]*dt)*W2[i])
+        # turn S and nu into time series
+        timestep = pd.Timedelta(dt*365, 'D')
+        assert timestep >= pd.Timedelta(1, 's'), 'Timestep too small (<1s).'
+        index = pd.date_range(start=pd.Timestamp('2024-01-01'), periods=N, freq=timestep)
+        # round index to nearest second (to avoid numerical rounding errors in pandas)
+        index = index.round('S')
+        S = pd.Series(S, index=index)
+        nu = pd.Series(nu, index=index)
+        return S, nu
+    
+    def plot(self, S: np.ndarray, nu: np.ndarray) -> None:
+        fig, axs = plt.subplots(2)
+        plt.xticks(rotation=45)
+        axs[0].plot(S)
+        axs[0].set_ylabel('Price') 
+        axs[0].tick_params(axis='x', labelrotation=45)   
+        axs[1].fill_between(range(len(nu)), nu, color='gray', alpha=0.5)
+        axs[1].set_xlabel('Time')
+        axs[1].set_ylabel('Volatility')
+        axs[1].tick_params(axis='x', labelrotation=45)
+        plt.suptitle(f'Heston Price Path ($\\mu={self.mu}, \\kappa={self.kappa}, \\theta={self.theta}, \\sigma={self.sigma}, \\rho={self.rho}, \\nu_0={self.nu0}$)')
+        plt.tight_layout()
+        plt.show()
+
---
+++ src/utils/rv_estimators.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+# Implements various estimators of realized variance as proposed in "A Tale of Two Time Scales: Determining Integrated Volatility With Noisy High-Frequency Data"
+
+
+def rv_naive(ts: pd.Series, tau: pd.Timedelta, verbose: bool=True):
+        n_orig = len(ts)
+        ts = ts.resample(tau).first()
+        n_new = len(ts)
+        if verbose:
+            print('\n')
+            print(f'Using naive estimator with tau={tau.total_seconds()}s.')
+            print(f'{n_orig} --> {n_new} data points.')
+            print(f'Used ffill {ts.isna().sum()} times.')
+        ts = ts.ffill()
+        rv = (ts.diff()**2).sum()
+        return rv
+
+def rv_multigrid(ts: pd.Series, tau: pd.Timedelta, K: int, verbose: bool=True):
+    # we resample such that t_{i+1}-t_i = tau and then we have K subgrids, such that each subgrid essentially
+    # uses tau_prime=tau*K (e.g. G_0 = {t_0, t_{K-1}, t_{2K-1}, ...})
+    n_orig = len(ts)
+    ts = ts.resample(tau).first()
+    n_new = len(ts)
+    if verbose:
+        print('\n')
+        print(f'Using multigrid estimator with tau={tau.total_seconds()}s and K={K}.')
+        print(f'{n_orig} --> {n_new} data points.')
+        print(f'Used ffill {ts.isna().sum()} times.')
+    ts = ts.ffill()
+    subgrids = [ts[start::K] for start in range(K)]
+    tau_prime = tau * K
+    rv_naive_estimators = []
+    for subgrid in subgrids:
+        rv_naive_est = rv_naive(subgrid, tau_prime, verbose=False)
+        rv_naive_estimators.append(rv_naive_est)
+    rv_naive_avg = np.mean(rv_naive_estimators)
+    return rv_naive_avg
+
+
+def rv_best(ts: pd.Series, tau: pd.Timedelta, K: int, verbose: bool=True):
+    n_orig = len(ts)
+    n_new = ts.resample(tau).first().count()
+    if verbose:
+        print('\n')
+        print(f'Using best estimator with tau={tau.total_seconds()}s and K={K}.')
+        print(f'{n_orig} --> {n_new} data points.')
+
+    rv_naive_est = rv_naive(ts, tau, verbose=False)
+    rv_multi_est = rv_multigrid(ts, tau, K, verbose=False)
+
+    n_bar = (n_new-K+1)/K
+    rv_best_est = rv_multi_est - (n_bar/n_new)*rv_naive_est
+    return rv_best_est
+     
